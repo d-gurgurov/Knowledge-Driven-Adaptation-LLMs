cdf61df4142c037e4425900feb9971fb231262e5
d-gurgurov <dgurgurov@gmail.com>
2025-01-10 10:17:23 +0100

    modified ner script

--- src/ner/1-train_ner.sh
+++ src/ner/1-train_ner.sh
@@ -12,25 +12,22 @@ all_languages=(
     'uig_Arab' 'urd_Arab' 'uzn_Latn' 'yor_Latn' 'zsm_Latn'
 )
 
+all_languages=('tha_Thai' 'uig_Arab' 'urd_Arab' 'uzn_Latn' 'yor_Latn' 'zsm_Latn')
 
-languages=('mlt_Latn')
 
 # Configurable parameters
 source="glot"   # Can be set to "glot"
 model="xlm-r"         # FacebookAI/xlm-roberta-base google-bert/bert-base-multilingual-cased
-configuration="baseline"  # Can be set to "seq_bn", "seq_bn_inv", or "lora"
-
-# xlm-r cn seq_bn_inv --> 'ell_Grek' 'slv_Latn' 'yor_Latn' 'zsm_Latn'
-# xlm-r glot seq_bn_inv --> 'ell_Grek' 'slv_Latn' 'yor_Latn' 'zsm_Latn'
+configuration="seq_bn_inv"  # Can be set to "seq_bn", "seq_bn_inv", or "lora"
 
 # Directory base path
-base_dir="/netscratch/dgurgurov/thesis/donwstream_tasks/ner"
+base_dir="/netscratch/dgurgurov/thesis/downstream_tasks/ner"
 
 # Loop over each language and seed
-for lang in "${languages[@]}"; do
+for lang in "${all_languages[@]}"; do
   for seed in 1 2 3; do
     # Define output directory
-    output_dir="${base_dir}/${source}/${model}/${configuration}/${lang}/${seed}"
+    output_dir="${base_dir}/${source}/${model}/fusion/${configuration}/${lang}/${seed}"
 
     # Create the output directory if it doesn't exist
     mkdir -p "$output_dir"
@@ -39,7 +36,9 @@ for lang in "${languages[@]}"; do
     python train_ner.py \
       --language "$lang" \
       --output_dir "$output_dir" \
-      --adapter_dir "/netscratch/dgurgurov/thesis/lang_adapters/$source/$model/$configuration" \
+      --adapter_dir "/ds/text/ConceptNet/lang_adapters/$source/$model/$configuration" \
+      --adapter_cn_dir "/ds/text/ConceptNet/lang_adapters/conceptnet/$model/$configuration" \
+      --adapter_glot_dir "/ds/text/ConceptNet/lang_adapters/glot/$model/$configuration" \
       --model_name "FacebookAI/xlm-roberta-base" \
       --learning_rate 1e-4 \
       --num_train_epochs 100 \
@@ -49,7 +48,7 @@ for lang in "${languages[@]}"; do
       --save_strategy "epoch" \
       --weight_decay 0.01 \
       --seed "$seed" \
-      --language_adapter "no" \
+      --language_adapter "fusion" \
       --adapter_source "$source"
   done
 done
